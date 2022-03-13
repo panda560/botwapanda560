52e1665405724de2a586786d3b12cc87dec4231b
asalbuat11 <90830308+ngasanaawan11@users.noreply.github.com>
2022-03-13 10:40:38 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.owner = ['6283128734012', '6287755080455'] // Letakan nomor kamu disini
+global.owner = ['6285857948496', '994400039395', '6281916017540'] // Letakan nomor kamu disini
 
 global.APIs = { // API Prefix
   // nama: 'https://website'
@@ -30,10 +30,10 @@ global.APIKeys = { // APIKey nya disini
 }
 
 // Sticker WM
-global.packname = 'arif️fb'
-global.author = 'amel'
+global.packname = 'uhuy'
+global.author = 'gans'
 
-global.wm = '© ariffb & melcanz'
+global.wm = '© uhuyBot'
 global.wait = '_*tunggu sedang diproses...*_'
 global.eror = '_*Server Error*_'
 global.benar = '✅'
