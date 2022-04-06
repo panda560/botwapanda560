69895cc8c99e8d571df67ec2172efae3ef20f83d
asalbuat11 <90830308+ngasanaawan11@users.noreply.github.com>
2022-04-06 22:45:56 +0800

    First Commit

--- plugins/delete.js
+++ plugins/delete.js
@@ -1,19 +1,19 @@
-// let handler = function (m) {
-//   if (!m.quoted) throw false
-//   let { chat, fromMe, id, isBaileys } = m.quoted
-//   if (!fromMe) throw false
-//   if (/Stikerin Broadcast/i.test(m.quoted.text)) throw 'Tidak bisa menghapus pesan broadcast!'
-//   if (!isBaileys) throw 'Pesan tersebut bukan dikirim oleh bot!'
-//   this.deleteMessage(chat, {
-//     fromMe,
-//     id,
-//     remoteJid: chat
-//   })
-// }
-// handler.help = ['del', 'delete']
-// handler.tags = ['info']
-// handler.command = /^del(ete)?$/i
+let handler = function (m) {
+  if (!m.quoted) throw false
+  let { chat, fromMe, id, isBaileys } = m.quoted
+  if (!fromMe) throw false
+  if (/Stikerin Broadcast/i.test(m.quoted.text)) throw 'Tidak bisa menghapus pesan broadcast!'
+  if (!isBaileys) throw 'Pesan tersebut bukan dikirim oleh bot!'
+  this.deleteMessage(chat, {
+    fromMe,
+    id,
+    remoteJid: chat
+  })
+}
+handler.help = ['del', 'delete']
+handler.tags = ['info']
+handler.command = /^delete$/i
 
-// handler.rowner = true
+handler.rowner = true
 
-// module.exports = handler
+module.exports = handler
