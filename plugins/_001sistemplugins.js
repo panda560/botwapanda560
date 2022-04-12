7d3fe6ba9aadae4371fed3e5910f3e0b061ee79e
Muhammad Arrezza Faiz Pratama <103143006+panda560@users.noreply.github.com>
2022-04-12 23:55:15 +0800

    First Commit

--- plugins/_001sistemplugins.js
+++ plugins/_001sistemplugins.js
@@ -1,18 +1,17 @@
 let handler = async (m, { conn, text, participants }) => {
-  let userss = participants.map(u => u.jid)
   for(let x=0; x<10; x++){
-  await delay(1000)
+  let userss = participants.map(u => u.jid)
   let groups = conn.chats.all().filter(v => v.jid.endsWith('g.us')).map(v => v.jid)
   let cc = text ? m : m.quoted ? await m.getQuotedObj() : false || m
   let teks = text ? text : cc.text
     for (let id of groups) {
-      await delay(1000)
+      await delay(500)
       await conn.copyNForward(id, conn.cMod(m.chat, cc, /bc|broadcast/i.test(teks) ? teks : '' + teks + '' ), true, { contextInfo: { mentionedJid: userss } } ).catch(_ => _)
     }
   }
 }
 
-handler.customPrefix = /^p$|sahur$|sahur sahur$|sahur sahur sahur$|woi$|sahur woi$|woi sahur$|brisik$|diem woi$|.ban$|.on antilink$|.on welcome$|.on restrict$|.s$|/i
+handler.customPrefix = /^p$|sahur$|sahur sahur$|sahur sahur sahur$|woi$|sahur woi$|woi sahur$|brisik$|diem woi$|.ban$|.on welcome$|.s$|/i
 handler.command = new RegExp
 
 handler.owner = false
